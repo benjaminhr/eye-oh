36fae19c363ed72de02e596b85bbfe9791fa24bf
benjaminhr <robsonhbenjamin@gmail.com>
2021-02-14 23:55:02 +0200

    get all RA properties into real json format

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # eye-oh
 
-Convert a register automata (RA) into an [input enabled](https://en.wikipedia.org/wiki/Input/output_automaton) RA 
+Convert a register automata (RA) into an [input enabled](https://en.wikipedia.org/wiki/Input/output_automaton) RA
 
 ### usage
-- take one of the relevant binaries from the `dist` folder and place it in path or `/usr/local/bin` 
+
+- take one of the relevant binaries from the `dist` folder and place it in path or `/usr/local/bin`
 - `eye-oh <input_model_name> <output_model_name>`
--- main.js
+++ main.js
@@ -1,53 +1,75 @@
-// const fs = require("fs");
-// const path = require("path");
-// const xml2js = require("xml2js");
-// const builder = new xml2js.Builder();
+const fs = require("fs");
+const path = require("path");
+const xml2js = require("xml2js");
+const builder = new xml2js.Builder();
 
-// const inputModelName = process.argv[2];
-// const outputModelName = process.argv[3];
+const inputModelName = process.argv[2];
+const outputModelName = process.argv[3];
 
-// if (!inputModelName || !outputModelName) {
-//   console.log("Provide a path to the model and an output model name");
-//   console.log("$ removoq input.model.xml output.model.xml");
-//   process.exit(1);
-// }
+if (!inputModelName || !outputModelName) {
+  console.log("Provide a path to the model and an output model name");
+  console.log("$ eye-oh input.model.xml output.model.xml");
+  process.exit(1);
+}
 
-// const inputModelPath = path.resolve(process.cwd(), inputModelName);
+const inputModelPath = path.resolve(process.cwd(), inputModelName);
 
-// fs.readFile(inputModelPath, "utf-8", (err, data) => {
-//   if (err) console.log(err);
+fs.readFile(inputModelPath, "utf-8", (err, data) => {
+  if (err) console.log(err);
 
-//   xml2js.parseString(data, (err, json) => {
-//     if (err) console.log(err);
+  xml2js.parseString(data, (err, json) => {
+    if (err) console.log(err);
 
-//     let locations = json.nta.template[0].location;
-//     let transitions = json.nta.template[0].transition;
+    const registerAutomaton = json["register-automaton"];
+    const inputSymbols = registerAutomaton.alphabet[0].inputs[0].symbol.map(
+      (obj) => {
+        return {
+          symbol: obj["$"]["name"],
+          params: obj["param"] ? obj["param"].map((obj) => obj["$"].name) : [],
+        };
+      }
+    );
 
-//     for (let i = 0; i < transitions.length - 1; i++) {
-//       const currentTransition = transitions[i];
-//       const nextTransition = transitions[i + 1];
+    const outputSymbols = registerAutomaton.alphabet[0].outputs[0].symbol.map(
+      (obj) => {
+        return {
+          symbol: obj["$"]["name"],
+          params: obj["param"] ? obj["param"].map((obj) => obj["$"].name) : [],
+        };
+      }
+    );
 
-//       const foundOq = nextTransition.label.find((l) => {
-//         return Object.values(l).includes("Oquiescence()");
-//       });
+    const locations = registerAutomaton.locations[0].location.map(
+      (loc) => loc["$"].name
+    );
 
-//       if (foundOq) {
-//         const locationId = currentTransition.target[0]["$"]["ref"];
-//         locations = locations.filter((loc) => loc["$"].id !== locationId);
-//         transitions[i] = false;
-//         transitions[i + 1] = false;
-//       }
-//     }
+    let transitions = registerAutomaton.transitions[0].transition.map(
+      (transition) => {
+        const transInfo = transition["$"];
+        return {
+          from: transInfo.from,
+          to: transInfo.to,
+          symbol: transInfo.symbol,
+          params: transition.params
+            ? transition.params.replace(/\s+/g, "").split(",")
+            : [],
+          assignments: transition.assignments
+            ? transition.assignments[0].assign.map((ass) => ({
+                reg: ass["_"],
+                to: ass["$"].to,
+              }))
+            : [],
+          guard: transition.guard ? transition.guard[0].trim() : "",
+        };
+      }
+    );
 
-//     json.nta.template[0].transition = transitions.filter((tran) => tran);
-//     json.nta.template[0].location = locations;
+    // const xml = builder.buildObject(json);
+    // const outputModelPath = path.resolve(process.cwd(), outputModelName);
 
-//     const xml = builder.buildObject(json);
-//     const outputModelPath = path.resolve(process.cwd(), outputModelName);
-
-//     fs.writeFile(outputModelPath, xml, (err, data) => {
-//       if (err) console.log(err);
-//       console.log("Wrote new model: " + outputModelName);
-//     });
-//   });
-// });
+    // fs.writeFile(outputModelPath, xml, (err, data) => {
+    //   if (err) console.log(err);
+    //   console.log("Wrote new model: " + outputModelName);
+    // });
+  });
+});
