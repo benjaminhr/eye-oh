b50af596550afc2ef5adcc0e11216c19c587d07d
benjaminhr <robsonhbenjamin@gmail.com>
2021-02-22 11:24:53 +0200

    add commander for cli flags, and option component running

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
 const fs = require("fs");
 const path = require("path");
-const utils = require("./src/utils");
+const { program } = require("commander");
 
+const utils = require("./src/utils");
 const inputEnable = require("./src/inputEnable");
 const addAlternatingIO = require("./src/addAlternatingIO");
 
-const inputModelName = process.argv[2];
-const outputModelName = process.argv[3];
+program
+  .requiredOption("-i, --input <path>", "path to input model")
+  .requiredOption("-o, --output <path>", "path to write output model")
+  .option("-a, --alternating-only", "only run alternating i/o component")
+  .option("-e, --input-enabling-only", "only run input enabling component");
 
-if (!inputModelName || !outputModelName) {
-  console.log("Provide a path to the model and an output model name");
-  console.log("$ eye-oh input.model.xml output.model.xml");
-  process.exit(1);
-}
+program.parse(process.argv);
 
+const options = program.opts();
+
+const inputModelName = options.input;
 const inputModelPath = path.resolve(process.cwd(), inputModelName);
+
+const outputModelName = options.output;
 const outputModelPath = path.resolve(process.cwd(), outputModelName);
 
 if (!fs.existsSync(inputModelPath)) {
@@ -25,8 +30,18 @@ if (!fs.existsSync(inputModelPath)) {
 (async () => {
   try {
     const JSONModel = await utils.getRegisterXML(inputModelPath);
-    const alternatingIOModel = addAlternatingIO(JSONModel);
-    const finalModel = inputEnable(alternatingIOModel);
+
+    let finalModel;
+    if (options.alternatingOnly) {
+      console.log("ONLY RUNNING ALTERNATING I/O COMPONENT");
+      finalModel = addAlternatingIO(JSONModel);
+    } else if (options.inputEnablingOnly) {
+      console.log("ONLY RUNNING INPUT ENABLING COMPONENT");
+      finalModel = inputEnable(JSONModel);
+    } else {
+      const alternatingIOModel = addAlternatingIO(JSONModel);
+      finalModel = inputEnable(alternatingIOModel);
+    }
 
     utils.writeModel(outputModelPath, finalModel);
     console.log("Wrote new model: " + outputModelName);
